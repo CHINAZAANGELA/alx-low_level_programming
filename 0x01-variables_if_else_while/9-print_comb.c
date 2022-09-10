887ea706604b9b0fe421c145a1b5be9d51beb031
CHINAZAANGELA <ibentaangela@gmail.com>
2022-09-10 15:06:47 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Success
+ */
+int mai(void)
+
+{
+	int number;
+
+	for (number =48; number <= 57; number++)
+	{
+		putchar(number);
+		{
+			break;
+		}
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
