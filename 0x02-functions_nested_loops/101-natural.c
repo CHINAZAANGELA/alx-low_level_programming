0351a842dd59e9011ae15289482692ddf412851e
CHINAZAANGELA <ibentaangela@gmail.com>
2022-09-14 19:56:39 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a, b;
+	{
+		for (a = 1; a < 1024; a++)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
